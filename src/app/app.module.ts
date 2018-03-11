fa0568645e10a36f1e411fbe7b079fad846783b4
tomvin6 <tomvin6@gmail.com>
2018-03-11 20:14:35 +0200

    adding auth, need backend to enable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,16 @@ import { APP_BASE_HREF } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule, HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import { CoreModule } from './@core/core.module';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ThemeModule } from './@theme/theme.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NbAuthModule, NbEmailPassAuthProvider} from '@nebular/auth';
+import {AuthGuard} from './auth-guard.service';
+import { NB_AUTH_PROVIDERS, NB_AUTH_TOKEN_CLASS, NbAuthJWTToken } from '@nebular/auth';
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,10 +29,84 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgbModule.forRoot(),
     ThemeModule.forRoot(),
     CoreModule.forRoot(),
+    NbAuthModule.forRoot({
+      providers: {
+        email: {
+          service: NbEmailPassAuthProvider,
+          config: {
+            baseEndpoint: 'http://localhost:3000',
+            login: {
+              alwaysFail: false,
+              rememberMe: true,
+              endpoint: '/auth/login',
+              method: 'post',
+              redirect: {
+                success: '/pages',
+                failure: null,
+              },
+              defaultErrors: ['Login/Email combination is not correct, please try again.'],
+              defaultMessages: ['You have been successfully logged in.'],
+            },
+            register: {
+              alwaysFail: false,
+              rememberMe: true,
+              endpoint: '/auth/register',
+              method: 'post',
+              redirect: {
+                success: '/pages',
+                failure: null,
+              },
+              defaultErrors: ['Something went wrong, please try again.'],
+              defaultMessages: ['You have been successfully registered.'],
+            },
+            logout: {
+              alwaysFail: false,
+              endpoint: '/auth/logout',
+              method: 'post',
+              redirect: {
+                success: '/',
+                failure: null,
+              },
+              defaultErrors: ['Something went wrong, please try again.'],
+              defaultMessages: ['You have been successfully logged out.'],
+            },
+            requestPass: {
+              endpoint: '/auth/request-pass',
+              method: 'post',
+              redirect: {
+                success: '/',
+                failure: null,
+              },
+              defaultErrors: ['Something went wrong, please try again.'],
+              defaultMessages: ['Reset password instructions have been sent to your email.'],
+            },
+            resetPass: {
+              endpoint: '/auth/reset-pass',
+              method: 'put',
+              redirect: {
+                success: '/',
+                failure: null,
+              },
+              resetPasswordTokenKey: 'reset_password_token',
+              defaultErrors: ['Something went wrong, please try again.'],
+              defaultMessages: ['Your password has been successfully changed.'],
+            },
+            token: {
+              key: 'token'
+            },
+            messages: {
+              key: 'messages'
+            },
+          },
+        },
+      },
+      forms: {},
+    }),
   ],
   bootstrap: [AppComponent],
   providers: [
-    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: APP_BASE_HREF, useValue: '/' }, AuthGuard,
+    { provide: NB_AUTH_PROVIDERS, useValue: NbAuthJWTToken },
   ],
 })
 export class AppModule {
