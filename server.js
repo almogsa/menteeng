ba055722ef693024c912a153e83202e0224a7e0e
sadea <almog@hpe.com>
2018-03-16 00:54:21 +0200

    delete backend

--- server.js
+++ server.js
@@ -1,28 +1,9 @@
-var express  = require('express');
-var app      = express();                               // create our app w/ express
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var cors = require('cors');
-var path = require("path");
+//Install express server
+const express = require('express');
+const app = express();
 
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-// app.use(methodOverride());
-app.use(cors());
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-const root = './';
-
-//app.use(express.static('www'));
-app.use(express.static(path.join(root, 'dist')));
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5000);
