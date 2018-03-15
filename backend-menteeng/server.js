fc34e76638e470208ce1d21bcd76c7adacc5a222
sadea <almog@hpe.com>
2018-03-15 23:19:21 +0200

    update server

--- backend-menteeng/server.js
+++ backend-menteeng/server.js
@@ -4,6 +4,28 @@ var app = express();
 var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
 
+
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var cors = require('cors');
+var path = require("path");
+
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+// app.use(methodOverride());
+app.use(cors());
+//app.use(bodyParser.json()); // define that what should we get in our parser should be json
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+const root = './';
+
+
 var messages = [{text: 'some text 1', owner: 'Tim'}, {text: 'other text', owner: 'Jane'}]
 var users = [
                 {firstName: 'Tom', email: 't@t.com', password : 't@t.com', id : 0, position: 'manager'},
@@ -11,14 +33,6 @@ var users = [
             ]; // in the future is DB instead of memory
 var loggedInUsers = [
             ]; // in the future is DB instead of memory
-
-app.use(bodyParser.json()); // define that what should we get in our parser should be json
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-})
-
 // create routers
 var api = express.Router(); // create a new router
 var auth = express.Router(); // create a new router
@@ -111,6 +125,13 @@ auth.post('/logout', (req, res) => {
 
 
 // start server
-let defaultPort = 3000;
-console.log('server listening to connection in port: ' + defaultPort);
-app.listen(defaultPort);
+//let defaultPort = 3000;
+//console.log('server listening to connection in port: ' + defaultPort);
+//app.listen(defaultPort);
+
+
+app.use(express.static(path.join(root, 'src')));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
