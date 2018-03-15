59d58ea895ee8e595c0fbf3ea1dbf69f5be7ea1f
sadea <almog@hpe.com>
2018-03-15 22:55:28 +0200

    DummyService

--- src/app/@core/data/users.service.ts
+++ src/app/@core/data/users.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 
 let counter = 0;
@@ -8,17 +8,25 @@ let counter = 0;
 export class UserService {
 
   private users = {
-    nick: { name: 'Nick Jones', picture: 'assets/images/nick.png' },
-    eva: { name: 'Eva Moor', picture: 'assets/images/eva.png' },
-    jack: { name: 'Jack Williams', picture: 'assets/images/jack.png' },
-    lee: { name: 'Lee Wong', picture: 'assets/images/lee.png' },
-    alan: { name: 'Alan Thompson', picture: 'assets/images/alan.png' },
-    kate: { name: 'Kate Martinez', picture: 'assets/images/kate.png' },
-    almog: {name: 'Almog Sade' , isStudent: true ,  email: 'almog@gmail.com', picture: 'assets/images/almog.png'},
-    almogsa: {name: 'Almog Sade' , isStudent: true , email: 'almog@gmail.com', picture: 'assets/images/almog.png'},
-    tom: {name: 'Tom Vaingart' , isStudent: true  , email: 'tom@gmail.com', picture: 'assets/images/tom.png'},
-    t: {name: 'Tom Vaingart' , isStudent: true  , class: '1' , category: '3' ,
-      email: 'tom@gmail.com', remark: 'remark' , picture: 'assets/images/almog.png'},
+    'nick@gmail.com': {name: 'Nick Jones', email: 'nick@gmail.com', picture: 'assets/images/nick.png'},
+    'eva@gmail.com': {name: 'Eva Moor', email: 'eva@gmail.com', picture: 'assets/images/eva.png'},
+    'jack@gmail.com': {name: 'Jack Williams', email: 'jack@gmail.com', picture: 'assets/images/jack.png'},
+    'lee@gmail.com': {name: 'Lee Wong', email: 'lee@gmail.com', picture: 'assets/images/lee.png'},
+    'alan@gmail.com': {name: 'Alan Thompson', email: 'alan@gmail.com', picture: 'assets/images/alan.png'},
+    'kate@gmail.com': {name: 'Kate Martinez', email: 'kate@gmail.com', picture: 'assets/images/kate.png'},
+    'almog@gmail.com': {
+      name: 'Almog Sade', isStudent: true,
+      email: 'almog@gmail.com', picture: 'assets/images/almog.png',
+    skills: [{category: 'Sport', sub_category: 'Football'}, {category: 'Sport', sub_category: 'Basketball'},
+      {category: 'Sport', sub_category: 'Tennis'}]},
+    'tom@gmail.com': {
+      name: 'Tom Vaingart', isStudent: true,
+      email: 'tom@gmail.com', picture: 'assets/images/tom.png'
+    },
+    't@gmail.com': {
+      name: 'Tami Moreno', isStudent: true, class: '1', category: '3',
+      email: 't@gmail.com', remark: 'remark', picture: 'assets/images/almog.png'
+    },
   };
 
   private userArray: any[];
@@ -29,6 +37,10 @@ export class UserService {
 
   getUsers(): Observable<any> {
     return Observable.of(this.users);
+  }
+
+  updateUser(user: any) {
+    this.users[user.email] = user;
   }
 
   getUserArray(): Observable<any[]> {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,21 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { APP_BASE_HREF } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import { CoreModule } from './@core/core.module';
+import {CoreModule} from './@core/core.module';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ThemeModule } from './@theme/theme.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NbAuthModule, NbEmailPassAuthProvider} from '@nebular/auth';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {ThemeModule} from './@theme/theme.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NbAuthModule, NbDummyAuthProvider, NbEmailPassAuthProvider} from '@nebular/auth';
 import {AuthGuard} from './auth-guard.service';
-import { NB_AUTH_PROVIDERS, NbAuthJWTToken } from '@nebular/auth';
+import {NB_AUTH_PROVIDERS, NbAuthJWTToken} from '@nebular/auth';
+import {AlmogDummyAuthProvider} from "./AlmogDummyProvider";
 
 @NgModule({
   declarations: [AppComponent],
@@ -32,70 +33,21 @@ import { NB_AUTH_PROVIDERS, NbAuthJWTToken } from '@nebular/auth';
     NbAuthModule.forRoot({
       providers: {
         email: {
-          service: NbEmailPassAuthProvider,
+          service: AlmogDummyAuthProvider,
           config: {
-            baseEndpoint: 'http://localhost:3000',
+            // delay: 3000,
             login: {
-              alwaysFail: false,
               rememberMe: true,
-              endpoint: '/auth/login',
-              method: 'post',
+              alwaysFail: false,
+              data: {
+                almog : true,
+              },
               redirect: {
                 success: '/pages',
-                failure: null,
+                failure: '/pages',
               },
               defaultErrors: ['Login/Email combination is not correct, please try again.'],
               defaultMessages: ['You have been successfully logged in.'],
-            },
-            register: {
-              alwaysFail: false,
-              rememberMe: true,
-              endpoint: '/auth/register',
-              method: 'post',
-              redirect: {
-                success: '/pages',
-                failure: null,
-              },
-              defaultErrors: ['Something went wrong, please try again.'],
-              defaultMessages: ['You have been successfully registered.'],
-            },
-            logout: {
-              alwaysFail: false,
-              endpoint: '/auth/logout',
-              method: 'post',
-              redirect: {
-                success: '/',
-                failure: null,
-              },
-              defaultErrors: ['Something went wrong, please try again.'],
-              defaultMessages: ['You have been successfully logged out.'],
-            },
-            requestPass: {
-              endpoint: '/auth/request-pass',
-              method: 'post',
-              redirect: {
-                success: '/',
-                failure: null,
-              },
-              defaultErrors: ['Something went wrong, please try again.'],
-              defaultMessages: ['Reset password instructions have been sent to your email.'],
-            },
-            resetPass: {
-              endpoint: '/auth/reset-pass',
-              method: 'put',
-              redirect: {
-                success: '/',
-                failure: null,
-              },
-              resetPasswordTokenKey: 'reset_password_token',
-              defaultErrors: ['Something went wrong, please try again.'],
-              defaultMessages: ['Your password has been successfully changed.'],
-            },
-            token: {
-              key: 'token',
-            },
-            messages: {
-              key: 'messages',
             },
           },
         },
@@ -105,8 +57,9 @@ import { NB_AUTH_PROVIDERS, NbAuthJWTToken } from '@nebular/auth';
   ],
   bootstrap: [AppComponent],
   providers: [
-    { provide: APP_BASE_HREF, useValue: '/' }, AuthGuard,
-    { provide: NB_AUTH_PROVIDERS, useValue: NbAuthJWTToken },
+    AlmogDummyAuthProvider,
+    {provide: APP_BASE_HREF, useValue: '/'}, AuthGuard,
+    {provide: NB_AUTH_PROVIDERS, useValue: NbAuthJWTToken},
   ],
 })
 export class AppModule {
