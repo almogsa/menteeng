15fd6596369decde00c05a40831eab6edd5ff75f
sadea <almog@hpe.com>
2018-03-21 13:30:57 +0200

    update matching

--- src/app/pages/tables/matching/matching.component.ts
+++ src/app/pages/tables/matching/matching.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {BodyOutputType, Toast, ToasterConfig, ToasterService} from "angular2-toaster";
+import {Router} from "@angular/router";
 @Component({
   selector: 'matching',
   templateUrl: './matching.component.html',
@@ -8,36 +10,74 @@ export class MatchingComponent implements OnInit {
 
   startMatching: boolean = false;
   title:string = 'Start matching !';
+  cb:any;
+  counter:number = 0;
+  config:any;
 
-  constructor() { }
+  constructor( private toasterService: ToasterService,
+              private router: Router) {
+    this.cb = (percent: number) : string => {
+     this.counter = percent;
+     return percent;
+    }
+  }
 
   ngOnInit() {
   }
-  /*formatSubtitle = (percent: number) : string => {
-    if(percent >= 100){
+   formatSubtitle = (percent: number) : string => {
+   this.counter = this.counter + 1;
+     percent = this.counter;
+    if(percent === 100){
+      setTimeout(() => {
+        this.router.navigate(['dashboard']);
+      },6000);
+      this.showToast();
+      this.title = "Done!"
+    // startMatching = true;
       return "Congratulations!"
-    }else if(percent >= 50){
-      return "Half"
-    }else if(percent > 0){
-      return "Just began"
+    }else if(percent >100) {
+      return "Congratulations!"
+    }
+    else if(percent >= 70){
+      return "Preparing results"
+    }else if(percent >= 40){
+      return "Analyzing Matching"
+    }
+    else if(percent >= 20){
+      return "Fetch Students"
+    }
+    else if(percent >= 10){
+      return "Fetch Mentors"
+    }
+    else if(percent > 0){
+      return "Start matching"
     }else {
       return "Not started"
     }
-  }*/
+  }
+
   startMatch() {
     this.startMatching = true;
+    this.counter = 0;
     this.title = 'in progress ...';
   }
-   /* formatSubtitle(percent: number)  {
-    console.log(number)
-    if(percent >= 100){
-      return "Congratulations!"
-    }else if(percent >= 50){
-      return "Half"
-    }else if(percent > 0){
-      return "Just began"
-    }else {
-      return "Not started"
+  private showToast(body?: string) {
+    let defaultBody = 'Matching has been successfully finished'
+    if (body && body.length > 0) {
+      defaultBody = body;
     }
-  }*/
+    this.config = new ToasterConfig({
+      positionClass: 'toast-top-center',
+      animation: 'fade'
+    });
+    const toast: Toast = {
+      type: 'success',
+      title: null,
+      body: defaultBody,
+      timeout: this.timeout,
+      showCloseButton: true,
+      bodyOutputType: BodyOutputType.TrustedHtml,
+    };
+    this.toasterService.popAsync(toast);
+  }
 }
